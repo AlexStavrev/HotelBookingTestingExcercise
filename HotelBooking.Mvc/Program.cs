f21b0455af069e42bd5a7efd7136849a184e761f
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-09-12 10:07:58 +0200

    Changed default controller and action
    Its now Booking as Home controller doesn't exist

--- HotelBooking.Mvc/Program.cs
+++ HotelBooking.Mvc/Program.cs
@@ -51,6 +51,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Bookings}/{action=Index}/{id?}");
 
 app.Run();
